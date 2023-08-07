f4a83744f3553bd5a1e54d97473dd4c98e94e23c
everyt <everytea.time@gmail.com>
2023-08-07 17:40:20 +0900

    initial commit

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,16 +5,8 @@ import { FaSearch } from 'react-icons/fa';
 function Header() {
   return (
     <div className='w-1200px ml-16 mt-6 my-6 flex flex-row justify-start'>
-      &nbsp;&nbsp;
-      <Image
-        className='relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert'
-        src='/next.svg'
-        alt='Next.js Logo'
-        width={155}
-        height={29}
-        priority
-      />
-      <div className='ml-16 mt-2 w-48% h-9 rounded-lg border border-gray-700'>
+      <Image src='/next.svg' alt='Next.js Logo' width={155} height={29} />
+      <div className='ml-14 mt-2 w-48% h-9 rounded-lg border border-gray-700'>
         <input
           className='mt-0 m-1 ml-6 outline-none text-sm text-slate-500'
           type='text'
@@ -27,6 +19,7 @@ function Header() {
           <FaSearch />
         </button>
       </div>
+      <img className='ml-14' src='/qrcode.png' height='70' width='70' />
     </div>
   );
 }
