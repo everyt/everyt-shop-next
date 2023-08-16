481bcf1b29aca8f07ea25651668d5ea7b0216340
Everyhongcha <80094147+everyt@users.noreply.github.com>
2023-08-16 19:05:25 +0900

    initial commit

--- components/Item.tsx
+++ components/Item.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+'use client';
+import React, { useState } from 'react';
 
 type ItemProps = {
   children: string;
@@ -9,30 +10,38 @@ type ItemProps = {
 };
 
 function Item({ children, img, name, price, category }: ItemProps) {
+  let [onHover, setOnHover] = useState(false);
   return (
-    <div className='item h-50 relative ml-2 mt-8 w-48 overflow-hidden rounded-2xl border shadow-md'>
+    <div
+      className='item h-50 relative ml-2 mt-8 w-48 overflow-hidden rounded-2xl border shadow-md'
+      onMouseEnter={() => setOnHover(true)}
+      onMouseLeave={() => setOnHover(false)}
+    >
       {category.length > 2 ? (
         category.length > 4 ? (
-          <p className='absolute ml-26 mt-26 h-5 w-60 rounded-md bg-neutral-700 pl-2 pr-2 font-LINEBd text-white'>
+          <p className='absolute z-10 ml-26 mt-26 h-5 w-60 rounded-md bg-neutral-700 pl-2 pr-2 font-LINEBd text-white'>
             {category}
           </p>
         ) : (
-          <p className='absolute ml-30 mt-26 h-5 w-40 rounded-md bg-neutral-700 pl-2 pr-2 font-LINEBd text-white'>
+          <p className='absolute z-10 ml-30 mt-26 h-5 w-40 rounded-md bg-neutral-700 pl-2 pr-2 font-LINEBd text-white'>
             {category}
           </p>
         )
       ) : (
-        <p className='absolute ml-37 mt-26 h-5 w-20 rounded-md bg-neutral-700 pl-2 pr-2 font-LINEBd text-white'>
+        <p className='absolute z-10 ml-37 mt-26 h-5 w-20 rounded-md bg-neutral-700 pl-2 pr-2 font-LINEBd text-white'>
           {category}
         </p>
       )}
-      <div className='rounded-t-2xl bg-neutral-200 pb-2 after:absolute after:h-2 after:bg-emerald-300 after:pr-5'>
-        <img
-          className='flex-auto rounded-t-2xl'
-          src={img}
-          alt='Item'
-        ></img>
-      </div>
+      <img
+        className='flex-auto rounded-t-2xl'
+        src={img}
+        alt='Item'
+      ></img>
+      {onHover ? (
+        <div className='rounded-t-2xl bg-neutral-200 pb-2 after:absolute after:z-0 after:h-2 after:animate-slidein after:bg-emerald-300 after:pr-5'></div>
+      ) : (
+        <div className='rounded-t-2xl bg-neutral-200 pb-2 after:absolute after:z-0 after:h-2 after:animate-slideout after:bg-emerald-300 after:pr-5'></div>
+      )}
       <div className='item-info flex h-24 content-center justify-center rounded-b-2xl bg-white'>
         <div className='mt-1 h-20 w-44'>
           <p className='item-name ml-1 font-LINERg text-xl text-neutral-700'>{name}</p>
